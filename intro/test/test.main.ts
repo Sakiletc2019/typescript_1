ec92ed1db1a1ac67117b439e32bbf56cb2b4a871
Sakiletc2019 <Sakiletc2019@gmail.com>
2025-01-11 16:22:58 +0530

    main.ts is pass on 'test.main.ts'✅

--- intro/test/test.main.ts
+++ intro/test/test.main.ts
@@ -1,1 +1,8 @@
 //test main.ts
+import { greeting } from "../src/main.ts";
+import { assertEquals } from "jsr:@std/assert";
+
+Deno.test("Greeting function",()=>{
+    const result=greeting();
+    assertEquals(result, "Hello TypeScript + 🦕");
+});

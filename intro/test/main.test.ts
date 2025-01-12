fd04bc1d1a74dbbd0b7ce6af2c19e2ce49d189e3
Sakiletc2019 <Sakiletc2019@gmail.com>
2025-01-12 14:46:47 +0530

    add testing result for 'main.ts' on 'main.test.ts' file

--- intro/test/main.test.ts
+++ intro/test/main.test.ts
@@ -8,5 +8,10 @@ Deno.test("Greeting function",()=>{
 });
 
 /*
+deno test intro/test/main.test.ts   
+Check file:///C:/Users/sakil/OneDrive/Desktop/my/Typescript+deno/typescript_1/intro/test/main.test.ts
+running 1 test from ./intro/test/main.test.ts
+Greeting function ... ok (0ms)
 
+ok | 1 passed | 0 failed (4ms)
 */

a064b28e3d288d68cddf1ddb6c85d52477043a3a
Sakiletc2019 <Sakiletc2019@gmail.com>
2025-01-12 15:47:03 +0530

    remore assert from 'intro/bench/add.bench.ts'

--- intro/bench/add.bench.ts
+++ intro/bench/add.bench.ts
@@ -1,8 +1,7 @@
 import { add } from "../src/add";
-import {assert, assertEquals} from "jsr:@std/assert";
+import { assertEquals} from "jsr:@std/assert";
 
 Deno.bench("add Benchmark",()=>{
-    assert(true);
     const result=add(3,4);
     
     assertEquals(result, 7);

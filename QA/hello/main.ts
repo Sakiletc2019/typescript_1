f0c53359fcf412d6a3062ea0d20b9038705c2565
Sakiletc2019 <Sakiletc2019@gmail.com>
2025-01-14 19:29:15 +0530

    add stand alone run properties

--- QA/hello/main.ts
+++ QA/hello/main.ts
@@ -3,12 +3,21 @@
 interface Param{
     message: string;
     sender?: string;
-}
+};
 
 type func=(params: Param)=> string;
 
 const greet: func =(params)=>{
     return `${params.sender} says: ${params.message}`
-}
+};
 
 export {greet};
+
+
+//for standalone run
+if (import.meta.main){
+    console.log(greet({
+        message: 'Hi Lucia',
+        sender: 'json'
+    }));
+};

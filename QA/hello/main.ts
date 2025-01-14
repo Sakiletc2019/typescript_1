4de8ad92973738e45a4f132065bdb0df712884c2
Sakiletc2019 <Sakiletc2019@gmail.com>
2025-01-14 18:41:51 +0530

    main.ts has written

--- QA/hello/main.ts
+++ QA/hello/main.ts
@@ -1,1 +1,14 @@
 //main.ts write main function
+
+interface Param{
+    message: string;
+    sender?: string;
+}
+
+type func=(params: Param)=> string;
+
+const greet: func =(params)=>{
+    return `${params.sender} says: ${params.message}`
+}
+
+export {greet};
